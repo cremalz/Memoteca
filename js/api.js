6ed7035833f1704ee127b1e875c97141bbf846a2
cremalz <lorenzocrematrabalho@gmail.com>
2026-08-21 16:59:18 -0300

    ciacao de banco de daos e de js da api para integracao com o codigo

---
+++ js/api.js
@@ -0,0 +1,13 @@
+const api = {
+    async buscarPensamentos(){
+        try {
+           const response = await fetch('http://localhost:3000/pensamentos')
+           return await response.json()
+        }
+        catch {
+            alert('Não foi possível buscar os pensamentos, tente novamente mais tarde.')
+        }
+    }
+}
+
+export default api;
