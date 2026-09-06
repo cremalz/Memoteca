ea5873ec4c28aa190faa94fe3234533050185232
cremalz <lorenzocrematrabalho@gmail.com>
2026-09-06 12:01:39 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -6,15 +6,7 @@ const ui = {
 
     try {
       const pensamentos = await api.buscarPensamentos()
-      pensamentos.forEach(pensamento => {
-        listaPensamentos.innerHTML += `
-          <li class="li-pensamento" data-id="${pensamento.id}">
-          <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-          <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-          <div class="pensamento-autoria">${pensamento.autoria}</div>
-          </li>
-        `
-      })
+      pensamentos.forEach(ui.adicionarPensamentoNaLista)
     }
     catch {
       alert('Erro ao renderizar pensamentos')
@@ -43,7 +35,7 @@ const ui = {
 li.appendChild(iconeAspas)
 li.appendChild(pensamentoConteudo)
 li.appendChild(pensamentoAutoria)
-
+listaPensamentos.appendChild(li)
 
   }
 }
