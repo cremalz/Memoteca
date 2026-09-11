01fab25829d281e5a1a120c8ebe360980d554605
cremalz <lorenzocrematrabalho@gmail.com>
2026-09-10 22:57:13 -0300

    ajuste de codigo correção gera e criação de circunstancia para adição de pensamentos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-import ui from "./ui.js"
+import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  ui.renderizarPensamentos()
-})
+  const formularioPensamento = document.getElementById("pensamento-form");
+
+  if (formularioPensamento) {
+    formularioPensamento.addEventListener(
+      "submit",
+      manipularSubmissaoFormulario,
+    );
+  }
+
+  ui.renderizarPensamentos();
+});
+
+async function manipularSubmissaoFormulario(evento) {
+  evento.preventDefault();
+
+  const id = document.getElementById("pensamento-id").value;
+  const conteudo = document.getElementById("pensamento-conteudo").value;
+  const autoria = document.getElementById("pensamento-autoria").value;
+
+  try {
+    await api.salvarPensamentos({ conteudo, autoria });
+    ui.renderizarPensamentos();
+  } catch {
+    alert("Erro ao salvar pensamento");
+  }
+}
