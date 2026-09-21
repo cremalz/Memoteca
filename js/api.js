ea2d71f3445c9c62b203bcabd7390f8dba54d30a
cremalz <lorenzocrematrabalho@gmail.com>
2026-09-20 21:37:54 -0300

    ajuste Alterando dados com o PUT

--- js/api.js
+++ js/api.js
@@ -22,10 +22,41 @@ const api = {
       return await response.json()
     }
     catch {
+      alert('Erro ao salvar pensamentos')
+      throw error
+    }
+  },
+
+ async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+      return await response.json()
+    }
+    catch {
       alert('Erro ao buscar pensamentos')
       throw error
     }
-  }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao editar pensamentos')
+      throw error
+    }
+  },
+
+
+
 }
 
 export default api
